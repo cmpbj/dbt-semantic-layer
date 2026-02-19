21b229be6c8e891733cd25bf89640d91444782f7
Carlos Magno Pinheiro Barreto Junior <carlosmag.barreto@gmail.com>
2026-02-19 01:51:54 +0000

    Feature: add generate schema name macro

--- models/0_bronze/stg_assets.sql
+++ models/0_bronze/stg_assets.sql
@@ -1,19 +1,22 @@
-with source_data as (
-    select *
-    from {{ source('raw', 'assets') }}
-),
-typed as (
-    select
-        safe_cast(created_at as timestamp) as created_at,
-        -- cast(asset_id as string) as asset_id,
-        cast(collection_status as string) as collection_status,
-        safe_cast(face_value as numeric) as face_value,
-        safe_cast(settled_at as timestamp) as settled_at,
-        safe_cast(due_date as date) as due_date,
-        cast(buyer_tax_id as string) as buyer_tax_id,
-        cast(seller_name as string) as seller_name,
-        upper(cast(buyer_state as string)) as buyer_state
-    from source_data
-)
+with
+    source_data as (
+        select *
+        from {{ source('raw', 'assets') }}
+    )
+
+    , typed as (
+        select
+            safe_cast(created_at as timestamp) as created_at
+            -- cast(asset_id as string) as asset_id,
+            , cast(collection_status as string) as collection_status
+            , safe_cast(face_value as numeric) as face_value
+            , safe_cast(settled_at as timestamp) as settled_at
+            , safe_cast(due_date as date) as due_date
+            , cast(buyer_tax_id as string) as buyer_tax_id
+            , cast(seller_name as string) as seller_name
+            , upper(cast(buyer_state as string)) as buyer_state
+        from source_data
+    )
+
 select *
 from typed
--- models/0_bronze/stg_ratings.sql
+++ models/0_bronze/stg_ratings.sql
@@ -1,13 +1,16 @@
-with source_data as (
-    select *
-    from {{ source('raw', 'ratings') }}
-),
-typed as (
-    select
-        safe_cast(created_at as timestamp) as created_at,
-        cast(tax_id as string) as tax_id,
-        cast(rating as string) as rating
-    from source_data
-)
+with
+    source_data as (
+        select *
+        from {{ source('raw', 'ratings') }}
+    )
+
+    , typed as (
+        select
+            safe_cast(created_at as timestamp) as created_at
+            , cast(tax_id as string) as tax_id
+            , cast(rating as string) as rating
+        from source_data
+    )
+
 select *
 from typed
