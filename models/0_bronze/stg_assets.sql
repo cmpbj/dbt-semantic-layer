38cef8a4c5c30a45b32610211938298ac1d0d6ca
cmpbj <magnocarlos3232@gmail.com>
2026-02-19 11:48:46 -0300

    Feature: rolback

--- models/0_bronze/stg_assets.sql
+++ models/0_bronze/stg_assets.sql
@@ -8,7 +8,7 @@ with
         select
             safe_cast(created_at as timestamp) as created_at
             -- cast(asset_id as string) as asset_id,
-, cast(collection_status as string) as collection_status
+            , cast(collection_status as string) as collection_status
             , safe_cast(face_value as numeric) as face_value
             , safe_cast(settled_at as timestamp) as settled_at
             , safe_cast(due_date as date) as due_date
